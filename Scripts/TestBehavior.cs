b1bc8e8b3b266074b343cf2fec7591400e4d92d0
Notes <33417990+DLsoul@users.noreply.github.com>
2022-05-17 19:25:06 +0800

    Add files via upload

---
+++ Scripts/TestBehavior.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Flags]
+enum ETest
+{
+	none = 0,
+	up = 1,
+	right = 2,
+	down = 4,
+	left = 8
+}
+public class TestBehavior : MonoBehaviour
+{
+
+	// Start is called before the first frame update
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
